03c91757fa9471db72faadc60049d52a4f06215c
dandymau_ <dandymau223@gmail.com>
2020-12-22 11:55:02 +0700

    Menambahkan jwt

--- app/User.php
+++ app/User.php
@@ -2,11 +2,12 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -39,15 +40,40 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function komentar() {
+    public function komentar()
+    {
         return $this->hasMany('App\Models\KomentarModel', 'user_id', 'user_id');
     }
 
-    public function nilai() {
+    public function nilai()
+    {
         return $this->hasMany('App\Models\NilaiModel', 'user_id', 'user_id');
     }
 
-    public function status_ujian() {
+    public function status_ujian()
+    {
         return $this->hasMany('App\Models\UserUjianStatusModel', 'user_id', 'user_id');
+    }
+
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
