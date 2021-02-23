030a67307583d457bacb82f6c2992fab92892944
dandymau_ <dandy.maulana99@gmail.com>
2021-02-24 00:31:23 +0700

    added home link to brand nav

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <a class="navbar-brand" href="#">MangaNihongo</a>
+    <a class="navbar-brand" href="{{route('index')}}">MangaNihongo</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
       aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
