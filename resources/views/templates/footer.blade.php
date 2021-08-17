d4e9a6121efd51d1b4c462e83032dd63512d0a8b
dandymau_ <dandy.maulana99@gmail.com>
2021-08-17 20:14:56 +0700

    update footer to copyright

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -2,23 +2,7 @@
     <div class="container">
     <div class="row justify-content-end text-center">
         <div class="col">
-        <a href="" class="">About Us</a>
-        </div>
-
-        <div class="col">
-        <a href="" class="">Terms & Conditions</a>
-        </div>
-
-        <div class="col">
-        <a href="" class="">Privacy Policy</a>
-        </div>
-
-        <div class="col">
-        <a href="" class="">F.A.Q</a>
-        </div>
-
-        <div class="col">
-        <a href="" class="">Contact Us</a>
+            <span>&#169 MANGO LEARNING CENTRE</span>
         </div>
     </div>
     </div>
