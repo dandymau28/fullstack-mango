077bf5b8c07db8b2fef1143f1ebb9808626f96af
dandymau_ <dandy.maulana99@gmail.com>
2021-08-17 22:45:00 +0700

    init user is admin middleware

---
+++ app/Http/Middleware/UserIsAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class UserIsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::user()->role !== 'guru') {
+            return redirect('index');
+        }
+
+        return $next($request);
+    }
+}
