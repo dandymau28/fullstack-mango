f8fa714d8db6fa6c584048f0c22298c1d5c43fc3
dandymau_ <dandy.maulana99@gmail.com>
2021-08-19 18:13:19 +0700

    remove withTrashed

--- app/Models/KomikModel.php
+++ app/Models/KomikModel.php
@@ -44,7 +44,7 @@ class KomikModel extends Model
     }
 
     public function buku() {
-        return $this->belongsTo('App\Models\BukuModel', 'buku_id', 'buku_id')->withTrashed();
+        return $this->belongsTo('App\Models\BukuModel', 'buku_id', 'buku_id');
     }
 
     public function komentar() {
